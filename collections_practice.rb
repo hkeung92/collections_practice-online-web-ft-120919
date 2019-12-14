781c5baf5c4f46120477ed0baef953b3c678052e
Ho Yin Keung <hykeung92@gmail.com>
2019-12-14 21:41:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,12 +20,16 @@ def sort_array_char_count(array)
   end
 end
 
+# def swap_elements(array)
+#   array[1..2] = array[1..2].sort do |a,b|
+#     if a==b || a<b || a>b 
+#       1
+#     end
+#   end
+#   array
+# end
 def swap_elements(array)
-  array[1..2] = array[1..2].sort do |a,b|
-    if a==b || a<b || a>b 
-      1
-    end
-  end
+  array[1], array[2] = array[2], array[1]
   array
 end
 
