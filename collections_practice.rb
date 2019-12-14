99d2f197500d7c3299d3d916b32a79d60f66c317
Ho Yin Keung <hykeung92@gmail.com>
2019-12-14 20:07:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,12 @@
 def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end
+end
+
+
+def sort_array_desc(array)
   array.sort do |b,a|
     b <=> a
   end
 end
-
-def sort_array_desc(array)
